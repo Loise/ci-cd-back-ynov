4ca3e56d7b799d20a27eeadcec1fcfbaf80894b7
Loïse Fenoll <admin@fingertips.fr>
2024-06-12 10:54:10 +0200

    express mongodb api

---
+++ app.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB = process.env.MONGODB_URL;
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String,
+  lastname: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+/**
+ * @description Get All note
+ * @route GET /utilisateurs
+ */
+const getAllUsers = (
+    async function (req, res, next) {
+      const users = await User.find({})
+      if (!users.length) return res.status(204).json({ message: "empty list" });
+
+      return res.status(200).json({ utilisateurs: users });
+    }
+  )
+ 
+
+const router = express.Router();
+
+router.route("/").get(getAllUsers);
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: "http://localhost:3000", 
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// api routes
+app.use("/users", router);
+
+module.exports = app;
